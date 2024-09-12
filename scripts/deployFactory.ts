845ce458530a34e15c06c4e1fb85efc31e4c586f
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-12 12:43:24 +0100

    Interaction script added

--- scripts/deployFactory.ts
+++ scripts/deployFactory.ts
@@ -7,9 +7,9 @@ async function main() {
   const factoryAddr = "0x545B2619DeD5A084F505a86BD2d4CeCDe62224ea";
   const GTKtokenAddr = "0x804383944275A1Fa7B460286b5e1C4dbF1cE3921";
 
-  // const walletFactory = await ethers.getContractAt("MultiSignatureFactory", factoryAddr);
-  const [owner ] = await hre.ethers.getSigners();
-  // const signers = [addr1, addr2, addr3];
+  const walletFactory = await ethers.getContractAt("MultiSignatureFactory", factoryAddr);
+  const [owner, addr1, addr2, addr3, addr4, addr5] = await hre.ethers.getSigners();
+  const signers = [addr1, addr2, addr3, addr4, addr5];
 
   // const signers = [
   //   "0x37A7610c8A62F1CB7d3b80fF5ADD8953d106E6a0",
@@ -18,24 +18,25 @@ async function main() {
   //   "0x59351e2B3F85534A2e1BF92a2424F37E180919D9"
   // ]
 
+
   //Create MultiSignature Wallet
-  // const createWallet = await walletFactory.createMultisigWallet(3, signers);
-  // const wallet = await createWallet.wait();
-  // console.log("Create MultiSig Clone:", wallet)
-  // const walletClone = await walletFactory.getMultiSigClones();
-  // console.log("New Wallet clone addresses: ", walletClone);
+  const createWallet = await walletFactory.createMultisigWallet(3, signers);
+  const wallet = await createWallet.wait();
+  console.log("Create MultiSig Clone:", wallet)
+  const walletClone = await walletFactory.getMultiSigClones();
+  console.log("New Wallet clone addresses: ", walletClone);
 
   //Interact with One Multisig Wallet.
-  // const walletClone1 = walletClone[0];
-  const walletClone1 = "0xc56345c0E430B949F21165B33E1a7970fe0EBa58";
+  const walletClone1 = walletClone[0];
+  // const walletClone1 = "0xc56345c0E430B949F21165B33E1a7970fe0EBa58";
   const sigWallet = await ethers.getContractAt("MultiSignature", walletClone1);
 
   //Transfer funds from GTK ERC20 token to clone contract first
-  // const gtkToken = await ethers.getContractAt("GTK", GTKtokenAddr);
-  // const amountToTransfer_ = ethers.parseUnits("5", 18);
-  // const trToken = await gtkToken.transfer(walletClone1, amountToTransfer_);
-  // trToken.wait();
-  // console.log("Transfer some GTK ERC Token to wallet:", trToken);
+  const gtkToken = await ethers.getContractAt("GTK", GTKtokenAddr);
+  const amountToTransfer_ = ethers.parseUnits("5", 18);
+  const trToken = await gtkToken.transfer(walletClone1, amountToTransfer_);
+  trToken.wait();
+  console.log("Transfer some GTK ERC Token to wallet:", trToken);
 
   //Interact with transfer function.
   const amountToTransfer = ethers.parseUnits("1", 18);
